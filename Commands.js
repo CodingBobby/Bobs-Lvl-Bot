491c1a07555f0a3e24599c07ba23ab6df394ba1e
Bob Walter <30275177+CodingBobby@users.noreply.github.com>
2018-04-05 19:43:41 +0200

    Update 0.0.5 05-04-18

--- Commands.js
+++ Commands.js
@@ -1,5 +1,5 @@
 // Commands.js by Bob™#0001
-// v 0.0.1 180328
+// v 0.0.5 180405
 
 
 // ——— Recource imports ———————————————————————————————————————————————————————— //
@@ -13,13 +13,18 @@ module.exports = function() {
 
 // ——— User commands ––––——————————————————————————————————————————————————————— //
 
-  this.uCmds = function(msg, cmd, args) {
+  this.uCmds = function(msg, cmd, args, disbot) {
 
-    // command block, if() statement can be dublicated as much as wanted
-    if(cmd == "name") {
-      // Something happens here
-      return 1;                                                               // Must return 1 to success search from main
+    if(cmd == 'me' || cmd == 'userinfo' || cmd == 'ui') {
+      cmdLog(cmd, args, msg);
+
+      const id = msg.author.id;
+      msg.channel.send(`\`\`\`cs\n# your ID: \n\t${id}\n# your Name: \n\t${disbot.users.get(id).username}\n# your Tag: \n\t${disbot.users.get(id).discriminator}\n# user since: \n\t${disbot.users.get(id).createdAt}\`\`\``);
+      console.log('\n> success');
+
+      return 1;
     }
+
 
     return;
   };
@@ -27,20 +32,83 @@ module.exports = function() {
 
 // ——— Owner commands –––——————————————————————————————————————————————————————— //
 
-  this.oCmds = function(msg, cmd, args) {
+  this.oCmds = function(msg, cmd, args, disbot) {
 
     if(msg.author.id !== owner.id)
-      return 0;                                                               // Must return 0 to break search from main
+      return 0;
 
-    // command block, if() statement can be dublicated as much as wanted
-    if(cmd == "name") {
-      // Something happens here
-      return 1;                                                               // Must return 1 to success search from main
+    if(cmd == 'top' || cmd == 'rank' || cmd == 'tr') {    // top 10 users by given argument
+      cmdLog(cmd, args, msg);
+
+      let usrArr = sortArr(gidArrSrch(msg,args[0]),2);
+      let topUsers = [];
+      if(usrArr.length >= 10)
+        count = 10;
+      else
+        count = usrArr.length;
+      for(var i = 0; i < count; i++) {
+        topUsers.push(usrArr[i]);
+      }
+      console.log(`\n> Top 10 on ${msg.guild.name} at ${args[0]}`);
+      console.log(topUsers);
+      const topList = (topUsers) => {
+        let msg = "";
+        for(var i = 0; i < topUsers.length; i++) {
+          let id = disbot.users.get(topUsers[i][0]);
+          msg += `**\n${i+1}:**\t${id.username}#${id.discriminator}`;
+        }
+        return msg;
+      }
+      msg.channel.send(topList(topUsers));
+      console.log('\n> success');
+
+      return 1;
     }
 
     return;
   };
+  
+// END of command section
 };
 
+
+// ——— Functions ––––––––––————————————————————————————————————————————————————— //
+
+function gidArrSrch(msg, search) {  // fetch profiles of all users in current guild
+  const gldUsrs = (prfls,glds) =>
+    Object.entries(prfls).filter(([user,attrs]) =>
+      Object.values(attrs.gld[glds] || {}).length);
+  const users = gldUsrs(profiles,msg.guild.id);
+  let newArr = [];
+  for(var user of users) {
+    const id = user[0];
+    const data = user[1];
+    let srchVal = data.gld[msg.guild.id][search];
+    newArr.push([id,srchVal]);
+  }
+  return newArr;
+}
+
+function sortArr(arr, s) {          // sort arr[][] for s. element
+  let swapp;
+  let n = arr.length - 1;
+  s -= 1;          // element to sort for
+  do {             // bubble sort
+    swapp = false;
+    for(var i = 0; i < n; i++) {
+      if(arr[i][s] < arr[i+1][s]) {
+        let tmp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = tmp;
+        swapp = true;
+      }
+    } n--;
+  } while (swapp); // stops when everything is sorted
+  return arr;
+}
+
+function cmdLog(cmd, args, msg) {   // general command log in console
+  console.log(`\n> COMMAND [${cmd}] with ARGUMENTS [${args}]\n> called by: ${msg.author.username}#${msg.author.discriminator}`)
+}
 
 // ———––––––––––——————————————————————————————————————————————————————————————— //
